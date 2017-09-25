183b0a48249defeaea59fed8781077736a82e1a4
K.C.Ashish Kumar <kcak11@gmail.com>
2017-09-25 10:30:16 +0530

    Update plugin.js

--- gitbook/gitbook-plugin-livereload/plugin.js
+++ gitbook/gitbook-plugin-livereload/plugin.js
@@ -2,7 +2,7 @@
   var newEl = document.createElement('script'),
       firstScriptTag = document.getElementsByTagName('script')[0];
 
-  if (firstScriptTag) {
+  if (false && firstScriptTag) {
     newEl.async = 1;
     newEl.src = '//' + window.location.hostname + ':35729/livereload.js';
     firstScriptTag.parentNode.insertBefore(newEl, firstScriptTag);
