df2c2bd00e9ed9f1dff9f1f7aa626033cd5c1d94
K.C.Ashish Kumar <kcak11@gmail.com>
2017-10-08 11:59:44 +0530

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -46,14 +46,14 @@
 	abtContainer.innerHTML = "About:<br/><br/>This documentation was compiled/authored by<br/><span style=\"font-weight:bold;\">K.C.Ashish Kumar</span><br/><br/>For more information please visit <a href=\"http://www.ashishkumarkc.com\" target=\"_blank\">http://www.ashishkumarkc.com</a><br/><br/><br/><img src=\"images/the-k.png\" style=\"width:198px;position:absolute;left:50%;margin-left:-99px;\" />";
 	document.querySelectorAll(".book-summary")[0].appendChild(abtContainer);
 
-	var gtagScript=document.createElement("script");
-	gtagScript.setAttribute("async","async"):
-	gtagScript.type="text/javascript";
-	gtagScript.src="https://www.googletagmanager.com/gtag/js?id=UA-98865610-4";
+	var gtagScript = document.createElement("script");
+	gtagScript.setAttribute("async", "async");
+	gtagScript.type = "text/javascript";
+	gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=UA-98865610-4";
 	document.getElementsByTagName("head")[0].appendChild(gtagScript);
-	
-	var gtagSrc=document.createElement("script");
+
+	var gtagSrc = document.createElement("script");
 	gtagSrc.appendChild(document.createTextNode(decodeURIComponent("window.dataLayer%20%3D%20window.dataLayer%20%7C%7C%20%5B%5D%3B%0A%20%20function%20gtag()%7BdataLayer.push(arguments)%3B%7D%0A%20%20gtag('js'%2C%20new%20Date())%3B%0A%0A%20%20gtag('config'%2C%20'UA-98865610-4')%3B")));
 	document.getElementsByTagName("head")[0].appendChild(gtagSrc);
-	
+
 }());
