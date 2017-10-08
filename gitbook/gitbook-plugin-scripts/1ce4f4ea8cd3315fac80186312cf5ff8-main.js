6dc3ba166d8043f662a2fd9ed1aad3b1188f4fc4
K.C.Ashish Kumar <kcak11@gmail.com>
2017-10-08 11:54:39 +0530

    Update 1ce4f4ea8cd3315fac80186312cf5ff8-main.js

--- gitbook/gitbook-plugin-scripts/1ce4f4ea8cd3315fac80186312cf5ff8-main.js
+++ gitbook/gitbook-plugin-scripts/1ce4f4ea8cd3315fac80186312cf5ff8-main.js
@@ -7,19 +7,22 @@
 	var checkAndAdjust = function() {
 		var bookBody = document.querySelectorAll(".book-body")[0];
 		var bodyInner = document.querySelectorAll(".book-body .body-inner")[0];
-		if (bodyInner && bookBody) {
-			bookBody.scrollTop = 0;
-			bodyInner.scrollTop = 0;
+		var bookHeaderActions = document.querySelectorAll(".book-header .js-toolbar-action")[0];
+		if (bookHeaderActions) {
+			setTimeout(function() {
+				bookBody.scrollTop = 0;
+				bodyInner.scrollTop = 0;
+				document.documentElement.scrollTop = 0;
+				document.getElementsByTagName("body")[0].scrollTop = 0;
+				window.scrollTo(0, 0);
+			}, 605);
 		} else {
 			setTimeout(function() {
 				checkAndAdjust();
-			}, 605);
+			}, 242);
 		}
-
 	};
-	setTimeout(function() {
-		checkAndAdjust();
-	}, 605);
+	checkAndAdjust();
 
 	var introElem = document.querySelectorAll("li.chapter[data-level=\"1.1\"] a")[0];
 	if (introElem) {
@@ -43,4 +46,14 @@
 	abtContainer.innerHTML = "About:<br/><br/>This documentation was compiled/authored by<br/><span style=\"font-weight:bold;\">K.C.Ashish Kumar</span><br/><br/>For more information please visit <a href=\"http://www.ashishkumarkc.com\" target=\"_blank\">http://www.ashishkumarkc.com</a><br/><br/><br/><img src=\"images/the-k.png\" style=\"width:198px;position:absolute;left:50%;margin-left:-99px;\" />";
 	document.querySelectorAll(".book-summary")[0].appendChild(abtContainer);
 
+	var gtagScript=document.createElement("script");
+	gtagScript.setAttribute("async","async"):
+	gtagScript.type="text/javascript";
+	gtagScript.src="https://www.googletagmanager.com/gtag/js?id=UA-98865610-4";
+	document.getElementsByTagName("head")[0].appendChild(gtagScript);
+	
+	var gtagSrc=document.createElement("script");
+	gtagSrc.appendChild(document.createTextNode(decodeURIComponent("window.dataLayer%20%3D%20window.dataLayer%20%7C%7C%20%5B%5D%3B%0A%20%20function%20gtag()%7BdataLayer.push(arguments)%3B%7D%0A%20%20gtag('js'%2C%20new%20Date())%3B%0A%0A%20%20gtag('config'%2C%20'UA-98865610-4')%3B")));
+	document.getElementsByTagName("head")[0].appendChild(gtagSrc);
+	
 }());
